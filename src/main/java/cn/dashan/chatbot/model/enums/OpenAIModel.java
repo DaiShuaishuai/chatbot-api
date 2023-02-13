a5cab528cc27ae0b79e7ab28224a56f85273a5fd
DaiShuaishuai <81281057+DaiShuaishuai@users.noreply.github.com>
2023-02-13 13:23:33 +0800

    Update OpenAIModel.java

--- src/main/java/cn/dashan/chatbot/model/enums/OpenAIModel.java
+++ src/main/java/cn/dashan/chatbot/model/enums/OpenAIModel.java
@@ -5,10 +5,10 @@ package cn.dashan.chatbot.model.enums;
  * @since 2023/2/11
  */
 public enum OpenAIModel {
-    TEXT_DEVINCI_003(1,"text-davinci-003",4000),
-    TEXT_CURIE_001(2,"text-curie-001",2000),
-    TEXT_BABBAGE_001(3,"text-babbage-001",2000),
-    TEXT_ADA_001(4,"text-ada-001",2000)
+    TEXT_DEVINCI_003(1,"text-davinci-003",2048),
+    TEXT_CURIE_001(2,"text-curie-001",1024),
+    TEXT_BABBAGE_001(3,"text-babbage-001",1024),
+    TEXT_ADA_001(4,"text-ada-001",1024)
     ;
 
 
